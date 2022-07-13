30497499f15bea0ecb5535a2a17011bc807dc762
Ori Bibas <oribibas6@gmail.com>
2022-07-13 13:47:15 -0400

    added User package: User, UserRepository, UserRole, UserService

---
+++ src/main/java/User/UserRole.java
@@ -0,0 +1,6 @@
+package User;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/User/UserService.java
@@ -0,0 +1,22 @@
+package User;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND =
+            "User %s not found, please try again.";
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, username)));
+    }
+}
